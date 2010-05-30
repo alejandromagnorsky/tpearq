10427827d3e5403865a3307071fad3cd2ce8cff2
merchante.mariano <merchante.mariano@010153f6-898e-01a7-a79b-74ce43d45377>
2010-05-30 20:39:07 +0000

    putc() y printf()

--- src/libc.c
+++ src/libc.c
@@ -1,5 +1,6 @@
 #include "../include/kc.h"
 
+#define NULL 0
 
 /***************************************************************
 *k_clear_screen
@@ -9,33 +10,74 @@
 
 void k_clear_screen() 
 {
-	char *vidmem = (char *) 0xb8000;
-	unsigned int i=0;
-	while(i < (80*25*2))
-	{
-		vidmem[i]=' ';
+	int i=0;
+        while(i < (80*25))
+        {
+		putc(' ');
 		i++;
-		vidmem[i]=WHITE_TXT;
-		i++;
-	};
+        };
+	__CONSOLE_PTR_ = 0;
 }
+
+int putc( int character ){
+	char * vidmem = (char*) 0xb8000;
+
+
+	// Si el puntero en la consola no es par, esta corrupto
+	if(__CONSOLE_PTR_ % 2 != 0)
+		return -1;
+
+	if( __CONSOLE_PTR_ >= 80*25*2)
+		__CONSOLE_PTR_ = 0;
+
+	if(character == '\n'){
+		int row = (__CONSOLE_PTR_/160) % 25;
+		__CONSOLE_PTR_ = (80*(row+1)*2)-2;
+		return character;
+	}
+
+
+	vidmem[__CONSOLE_PTR_++] = character;
+	vidmem[__CONSOLE_PTR_++] = WHITE_TXT;
+
+	return character;
+} 
+
+int printf(const char * str){
+	int c;
+	int wait = 0;
+	for(c=0;str[c] != NULL;c++){
+		if(wait){
+			switch(str[c]){
+
+			case 'd':
+				printf("NUMERO");
+				break;
+			case 'c':
+				putc(str[c]);
+				break;
+			case 's':
+				printf("STRING");
+				break;
+			default:
+				break;
+			}
+
+			wait = 0;
+		} else {
+			if(str[c] == '%')
+				wait = 1;
+			else 
+				putc(str[c]);
+		}
+	}
+}
+
+
 /* Imprime símbolo de sistema */
 void printSystemSymbol() 
 {
-	char *vidmem = (char *) 0xb8000;
-	unsigned int i=0;
-	vidmem[i++]='A';
-	vidmem[i++]=WHITE_TXT;
-	vidmem[i++]='r';
-	vidmem[i++]=WHITE_TXT;
-	vidmem[i++]='q';
-	vidmem[i++]=WHITE_TXT;
-	vidmem[i++]=':';
-	vidmem[i++]=WHITE_TXT;
-	vidmem[i++]='/';
-	vidmem[i++]=WHITE_TXT;
-	vidmem[i++]='>';
-
+	printf("Pikachu:/>");
 }
 
 
