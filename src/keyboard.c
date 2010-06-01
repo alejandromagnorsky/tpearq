48a3d8c9add9b4e70336c1a0312b277957135a1c
LegionarioDeAcero <LegionarioDeAcero@010153f6-898e-01a7-a79b-74ce43d45377>
2010-06-01 14:15:55 +0000

    teclado

--- src/keyboard.c
+++ src/keyboard.c
@@ -6,19 +6,13 @@ int __keyboard_buffer[KBUFFER_SIZE] = {0};
 int makeCodes[53] =	 {-5, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'', -5, '\b', '\t', 'q', 'w', 'e', 'r', 
 			't', 'y', 'u', 'i', 'o', 'p', -5, '+', '\n', -5, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',
 			'l', -5, '{', '}', -5, '<', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'};
-int breakCodes[53] =	 {-5, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'', -5, '\b', '\t', 'q', 'w', 'e', 'r', 
-			't', 'y', 'u', 'i', 'o', 'p', -5, '+', '\n', -5, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',
-			'l', -5, '{', '}', -5, '<', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'};
 
 void printKey(){
 	int index = _read_scancode() & 0xFF;
-	if (index > 0x80)
-		__keyboard_buffer[__KBUFFER_PTR_] = breakCodes[index-0x80-1];	
-	else{
+	if (index <= 83){
 		__keyboard_buffer[__KBUFFER_PTR_] = makeCodes[index-1];
 		printf("%c", __keyboard_buffer[__KBUFFER_PTR_]);
+		__KBUFFER_PTR_ = (__KBUFFER_PTR_+ 1) % KBUFFER_SIZE;
 	}
-
-	__KBUFFER_PTR_ = (__KBUFFER_PTR_+ 1) % KBUFFER_SIZE;
 }
 
