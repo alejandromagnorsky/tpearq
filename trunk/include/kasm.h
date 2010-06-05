421158e81153a321dd1e7d58b638078e244c13cc
alejandromagnorsky <alejandromagnorsky@010153f6-898e-01a7-a79b-74ce43d45377>
2010-06-05 14:40:35 +0000

    Remmapeado el PIC 20h interrupciones mas arriba. Tuve que borrar scanf porque no compilaba sino. La pueden obtener de la revision anterior.

--- trunk/include/kasm.h
+++ trunk/include/kasm.h
@@ -18,8 +18,11 @@ void		_Sti(void);	 /* Habilita interrupciones  */
 
 void		_print_time();
 
-void		_int_08_hand();      /* Timer tick */
-void		_int_09_hand();	     /* Teclado */
+void		_int_20_hand();      /* Timer tick */
+void		_int_21_hand();	     /* Teclado */
+
+int		_read_scancode();
+void		_out_pic(dword dest, dword origin); /* It does an OUT in the dest whit the value in the origin. The destiny must be: 0x20, 0x21, 0xA0 or 0xA1 */
 
 void 		_turn_cursor_on();
 void 		_turn_cursor_off();
