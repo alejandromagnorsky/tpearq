dddc9fc4a70944aab69b077b9ee97acbb732f5f8
alejandromagnorsky <alejandromagnorsky@010153f6-898e-01a7-a79b-74ce43d45377>
2010-06-08 23:49:33 +0000

    Agregadas mas excepciones

--- include/kasm.h
+++ include/kasm.h
@@ -18,6 +18,39 @@ void		_mascaraPIC2 (byte mascara);  /* Escribe mascara de PIC2 */
 void		_Cli(void);        /* Deshabilita interrupciones  */
 void		_Sti(void);	 /* Habilita interrupciones  */
 
+
+void		_int_00_hand();
+void		_int_01_hand();
+void		_int_02_hand();
+void		_int_03_hand();
+void		_int_04_hand();
+void		_int_05_hand();
+void		_int_06_hand();
+void		_int_07_hand();
+void		_int_08_hand();
+void		_int_09_hand();
+void		_int_0A_hand();
+void		_int_0B_hand();
+void		_int_0C_hand();
+void		_int_0D_hand();
+void		_int_0E_hand();
+void		_int_0F_hand();
+void		_int_10_hand();
+void		_int_11_hand();
+void		_int_12_hand();
+void		_int_13_hand();
+void		_int_14_hand();
+void		_int_15_hand();
+void		_int_16_hand();
+void		_int_17_hand();
+void		_int_18_hand();
+void		_int_19_hand();
+void		_int_1A_hand();
+void		_int_1B_hand();
+void		_int_1C_hand();
+void		_int_1D_hand();
+void		_int_1E_hand();
+void		_int_1F_hand();
 void		_int_20_hand();      /* Timer tick */
 void		_int_21_hand();	     /* Teclado */
 void		_int_80_hand(dword rd_wr, size_t fd, void * buffer, size_t count); /* Maneja el read y el write */
--- src/interrupts.c
+++ src/interrupts.c
@@ -4,131 +4,132 @@
 #include "../include/keyboard.h"
 #include "../include/interrupts.h"
 
-void int_00_hand(){
+void int_00(){
 	printf("Error al dividir por cero.\n");
+	__asm__("movl	$1, %ebx");
 }
 
-void int_01_hand(){
+void int_01(){
+	printf("Debug exception.\n");
+}
+
+void int_02(){
+	printf("Nonmaskable interrupt.\n");
+}
+
+void int_03(){
+	printf("Breakpoint.\n");
+}
+
+void int_04(){
+	printf("Overflow exception.\n");
+}
+
+void int_05(){
+	printf("Bounds check.\n");
+}
+
+void int_06(){
+	printf("Invalid opcode.\n");
+}
+
+void int_07(){
+	printf("Coprocessor not available.\n");
+}
+
+void int_08(){
+	printf("Double fault\n");
+}
+
+void int_09(){
 	printf("Reserved.\n");
 }
 
-void int_02_hand(){
+void int_0A(){
+	printf("Invalid TSS.\n");
+}
+
+void int_0B(){
+	printf("Segment not present.\n");
+}
+
+void int_0C(){
+	printf("Stack exception.\n");
+}
+
+void int_0D(){
+	printf("General protection\n");
+}
+
+void int_0E(){
+	printf("Page fault.\n");
+}
+
+void int_0F(){
 	printf("Reserved.\n");
 }
 
-void int_03_hand(){
+void int_10(){
+	printf("Coprocessor error.\n");
+}
+
+void int_11(){
 	printf("Reserved.\n");
 }
 
-void int_04_hand(){
+void int_12(){
 	printf("Reserved.\n");
 }
 
-void int_05_hand(){
+void int_13(){
 	printf("Reserved.\n");
 }
 
-void int_06_hand(){
+void int_14(){
 	printf("Reserved.\n");
 }
 
-void int_07_hand(){
+void int_15(){
 	printf("Reserved.\n");
 }
 
-void int_08_hand(){
+void int_16(){
 	printf("Reserved.\n");
 }
 
-void int_09_hand(){
+void int_17(){
 	printf("Reserved.\n");
 }
 
-void int_0A_hand(){
+void int_18(){
 	printf("Reserved.\n");
 }
 
-void int_0B_hand(){
+void int_19(){
 	printf("Reserved.\n");
 }
 
-void int_0C_hand(){
+void int_1A(){
 	printf("Reserved.\n");
 }
 
-void int_0D_hand(){
+void int_1B(){
 	printf("Reserved.\n");
 }
 
-void int_0E_hand(){
+void int_1C(){
 	printf("Reserved.\n");
 }
 
-void int_0F_hand(){
+void int_1D(){
 	printf("Reserved.\n");
 }
 
-void int_10_hand(){
+void int_1E(){
 	printf("Reserved.\n");
 }
 
-void int_11_hand(){
-	printf("Reserved.\n");
-}
-
-void int_12_hand(){
-	printf("Reserved.\n");
-}
-
-void int_13_hand(){
-	printf("Reserved.\n");
-}
-
-void int_14_hand(){
-	printf("Reserved.\n");
-}
-
-void int_15_hand(){
-	printf("Reserved.\n");
-}
-
-void int_16_hand(){
-	printf("Reserved.\n");
-}
-
-void int_17_hand(){
-	printf("Reserved.\n");
-}
-
-void int_18_hand(){
-	printf("Reserved.\n");
-}
-
-void int_19_hand(){
-	printf("Reserved.\n");
-}
-
-void int_1A_hand(){
-	printf("Reserved.\n");
-}
-
-void int_1B_hand(){
-	printf("Reserved.\n");
-}
-
-void int_1C_hand(){
-	printf("Reserved.\n");
-}
-
-void int_1D_hand(){
-	printf("Reserved.\n");
-}
-
-void int_1E_hand(){
-	printf("Reserved.\n");
-}
-
-void int_1F_hand(){
+void int_1F(){
 	printf("Reserved.\n");
 }
 
