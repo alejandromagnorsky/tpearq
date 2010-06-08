aae5765be80c9ee5bbb3037f425bd1905433021d
alejandromagnorsky <alejandromagnorsky@010153f6-898e-01a7-a79b-74ce43d45377>
2010-06-08 16:55:50 +0000

    Agregado inport() y modificado time para que sea un nuevo comando del shell

--- trunk/include/kasm.h
+++ trunk/include/kasm.h
@@ -5,7 +5,8 @@ kasm.h
 
 #include "defs.h"
 
-
+#ifndef _kasm_
+#define _kasm_
 unsigned int    _read_msw();
 
 void            _lidt (IDTR *idtr);
@@ -16,12 +17,11 @@ void		_mascaraPIC2 (byte mascara);  /* Escribe mascara de PIC2 */
 void		_Cli(void);        /* Deshabilita interrupciones  */
 void		_Sti(void);	 /* Habilita interrupciones  */
 
-void		_print_time();
-
 void		_int_20_hand();      /* Timer tick */
 void		_int_21_hand();	     /* Teclado */
 
 int		_read_scancode();
+int		_inport(dword port);
 void 		_outport(dword port, dword data);
 
 void 		_turn_cursor_on();
@@ -30,4 +30,4 @@ void		_move_cursor(unsigned int pos);
 void		_out_vga(int port, int value);
 
 void		_debug (void);
-
+#endif
