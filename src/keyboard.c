7361078067dca4522ac02d04db4e83a77030fc89
alejandromagnorsky <alejandromagnorsky@010153f6-898e-01a7-a79b-74ce43d45377>
2010-06-08 16:05:29 +0000

    Agregados los acentos para las minusculas. Las mayusculas con acentos no tiene ASCII

--- src/keyboard.c
+++ src/keyboard.c
@@ -2,42 +2,72 @@
 /*	PROBLEMAS
 **	TECLAS QUE NO TIRAN INTERRUPCIÓN:
 **		AltGr.	
-**	CASOS IGUALES
-**		| y 1
 **	AVERIGUAR EL SCAN CODE DE
 **		La tecla de al lado de la Ñ
-**		Shift+|
+**		Shift+| y |
+**		< y >
+**		}
+**	AVERIGUAR QUE HACER CON LAS TECLAS INSERT, INICIO, SUPR y FIN cuando el numPad esta activado
+**		
 */
 
 int __L_SHIFT_ = 0;	// Left Shift flag (0 = unabled)
 int __R_SHIFT_ = 0; // Right Shift flag (0 = unabled)
 int __CAPSLOCK_ = 0;	// Capslock flag (0 = unabled)
 int __NUMLOCK_ = 0; // NumLock flag (0 = unabled)
+int __TILDE_ = 0;
+int __EXTENDED_ = 0;
 
 int makeCodes[83] =	 {-1, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 39, 168, '\b', '\t', 'q', 'w', 'e', 'r', 
-			  't', 'y', 'u', 'i', 'o', 'p', -5, '+', '\n', -5, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
-                          164, '{', '}', 124, -5, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '-', -5, -5, -5, ' ', -5,
+			  't', 'y', 'u', 'i', 'o', 'p', '{', '+', '\n', -5, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
+                          164, -5, '}', 124, -5, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '-', -5, -5, -5, ' ', -5,
 			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 'N', 'S', 'H', 202, -1, '-', 185, 206, 204, '+', 'E',
 			  203, -1, 'I', 'D'};
 
 int shiftMakeCodes[83] = {-1, '!', '"', '#', '$', '%', '&', '/', '(', ')', '=', '\?', 173, '\b', '\t', 'Q', 'W', 'E', 'R', 
-			  'T', 'Y', 'U', 'I', 'O', 'P', -5, '*', '\n', -5, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 
-			  165, '[', ']', 167, -5, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ';', ':', '_', -5, -5, -5, ' ', -5,
+			  'T', 'Y', 'U', 'I', 'O', 'P', '[', '*', '\n', -5, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 
+			  165, -5, ']', 167, -5, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ';', ':', '_', -5, -5, -5, ' ', -5,
 			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 'N', 'S', 'H', 202, -1, '-', 185, 206, 204, '+', 'E',
 			  203, -1, 'I', 'D'};
 
 int numpadMakeCodes[13] = {'7', '8', '9', -1, '4', '5', '6', -1, '1', '2', '3', '0', '.'};
 
+int extendedMakeCodes[83] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+			     -1, -1, -1, -1, -1, -1, -1, -1, '\n', -5, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+                             -1, -1, -1, -1, -5, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 'H', 202, -1, '-', 185, 206, 204, -1, 'E',
+			     203, -1, 'I', 'D'};
+
 int getAscii(int scanCode){
 	int asciiCode = -1;
 	
+	//For scanCodes with more than 1 byte
+	if(scanCode & 0xFF00 == 0x0E00){
+		printf("asdasd");
+		__EXTENDED_ = 1;
+	}
+		
+	scanCode = scanCode & 0xFF;
+
+
+
 	if (scanCode == 170)
 		__L_SHIFT_ = 0;  
 	else if(scanCode == 182)
 		__R_SHIFT_ = 0;
-	// Si es un make code
+	// If the scanCode is a make code
 	else if (scanCode <= 128){
-		if(scanCode == 42)
+		if(__TILDE_){
+			if(isVocal(scanCode))
+				asciiCode = getTildeVocal(scanCode);
+			__TILDE_ = 0;
+		} else if(__EXTENDED_){
+			asciiCode = extendedMakeCodes[scanCode-1];
+			__EXTENDED_ = 0;
+		}
+		else if(scanCode == 40){
+			__TILDE_ = 1;
+		} else if(scanCode == 42)
 			__L_SHIFT_ = 1;
 		else if(scanCode == 54)
 			__R_SHIFT_ = 1;
@@ -56,7 +86,7 @@ int getAscii(int scanCode){
 			asciiCode = shiftMakeCodes[scanCode-1];
 		
 		else
-			asciiCode = makeCodes[scanCode-1];	
+			asciiCode = makeCodes[scanCode-1];
 	}
 	return asciiCode;
 }
@@ -71,3 +101,36 @@ char isNumber(int scanCode){
                  || (scanCode >= 75 && scanCode <= 77)
                  || (scanCode >= 79 && scanCode <= 83);
 }
+
+char isVocal(int scanCode){
+	return (makeCodes[scanCode-1] == 'e' || makeCodes[scanCode-1] == 'u' || 
+		makeCodes[scanCode-1] == 'i' || makeCodes[scanCode-1] == 'o' 
+		|| makeCodes[scanCode-1] == 'a');
+}
+
+char getTildeVocal(int scanCode){
+	int ans;
+	switch(makeCodes[scanCode-1]){
+		case 'e':
+			ans = 130;
+			break;
+		case 'u':
+			ans = 163;
+			break;
+		case 'i':
+			ans = 161;
+			break;
+		case 'o':
+			ans = 162;
+			break;
+		case 'a':
+			ans = 160;
+	}
+	if(__CAPSLOCK_ || __L_SHIFT_ || __R_SHIFT_)
+		ans = -1;
+	return ans;
+}
+
+
+
+
