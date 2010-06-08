4cd5964f7e0add09935d35459ada195e769b9daf
alejandromagnorsky <alejandromagnorsky@010153f6-898e-01a7-a79b-74ce43d45377>
2010-06-08 21:13:31 +0000

    Agregadas las rutinas de excepcion. La única que tiene un comando para el shell es la 0 y se cuelga por un tema del micro.

---
+++ trunk/include/interrupts.h
@@ -0,0 +1,43 @@
+/**********************
+interrupts.h
+**********************/
+
+#ifndef _interrupts_
+#define _interrupts_
+
+void int_00_hand();
+void int_01_hand();
+void int_02_hand();
+void int_03_hand();
+void int_04_hand();
+void int_05_hand();
+void int_06_hand();
+void int_07_hand();
+void int_08_hand();
+void int_09_hand();
+void int_0A_hand();
+void int_0B_hand();
+void int_0C_hand();
+void int_0D_hand();
+void int_0E_hand();
+void int_0F_hand();
+void int_10_hand();
+void int_11_hand();
+void int_12_hand();
+void int_13_hand();
+void int_14_hand();
+void int_15_hand();
+void int_16_hand();
+void int_17_hand();
+void int_18_hand();
+void int_19_hand();
+void int_1A_hand();
+void int_1B_hand();
+void int_1C_hand();
+void int_1D_hand();
+void int_1E_hand();
+void int_1F_hand();
+void int_20();
+void int_21();
+
+#endif
